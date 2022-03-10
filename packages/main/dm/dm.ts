1133f5874f947ebf221ed94dad287326f4c6c0b8
lclscofield <lclscofield@foxmail.com>
2022-03-10 21:10:28 +0800

    feat: 完善注册dm.dll到系统

--- packages/main/dm/dm.ts
+++ packages/main/dm/dm.ts
@@ -5,13 +5,15 @@
 import winax from 'winax'
 import { execSync } from 'child_process'
 import { ipcMain } from 'electron'
+import path from 'path'
 
 // 调用大漠插件
 function getDm () {
     try {
         return new winax.Object('dm.dmsoft')
     } catch (error) {
-        execSync('regsvr32 ../dll/dm.dll /s')
+        // 注册 dm.dll 到系统，再直接加载 dm.dmsoft
+        execSync(`regsvr32 ${path.resolve(__dirname, 'dm.dll')} /s`)
         return new winax.Object('dm.dmsoft')
     }
 }
--- packages/renderer/src/utils/dm.ts
+++ packages/renderer/src/utils/dm.ts
@@ -2,10 +2,6 @@
 /**
  * dm 函数，调用主进程 dm 插件
  */
-// import { createRequire } from 'module'
-// const require = createRequire(import.meta.url)
-// const { ipcRenderer } = require('electron')
-// import { ipcRenderer } from 'electron'
 
 export default async function (method: string, args: any[]) {
     return await window.ipcRenderer.invoke('dm', method, args)
