cf393cc21f0b2dfe1a5f9fad1b902aefd7082528
lclscofield <lclscofield@foxmail.com>
2022-03-10 22:14:34 +0800

    feat: 完善渲染进程中dm的调用

--- packages/renderer/src/components/HelloWorld.vue
+++ packages/renderer/src/components/HelloWorld.vue
@@ -1,68 +1,28 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 // import { useStore } from '../store/index'
 import dm from '../utils/dm'
 
-const send = () => {
-    dm('MoveTo', [0, 0])
+async function send (): Promise<void> {
+// 打开软件
+    const ret = await dm.runApp('cloudmusic', 1)
+    console.log(ret)
 }
 
 defineProps<{ msg: string }>()
 
-const c = ref(0)
+// const c = ref(0)
 // const store = useStore()
 // console.log(c.value, store.count)
 </script>
 
 <template>
-    <h1>{{ msg }}</h1>
-
-    <p>
-        Recommended IDE setup:
-        <a
-            href="https://code.visualstudio.com/"
-            target="_blank"
-        >VSCode</a>
-        +
-        <a
-            href="https://github.com/johnsoncodehk/volar"
-            target="_blank"
-        >Volar</a>
-    </p>
-
-    <p>
-        See
-        <code>README.md</code> for more information.
-    </p>
-
-    <p>
-        <a
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-        >Vite Docs</a>
-        |
-        <a
-            href="https://v3.vuejs.org/"
-            target="_blank"
-        >Vue 3 Docs</a>
-    </p>
-
     <button
         type="button"
         @click="send"
     >
-        count is: {{ c }}
+        打开网易云
     </button>
-    <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test hot module replacement.
-    </p>
-
-    <div class="xx">
-        <div class="xx-aa">
-            123
-        </div>
-    </div>
 </template>
 
 <style lang="scss" scoped>
@@ -82,9 +42,4 @@ code {
     color: #304455;
 }
 
-.xx {
-    &-aa {
-        color: #42b983;
-    }
-}
 </style>
--- packages/renderer/src/utils/dm.ts
+++ packages/renderer/src/utils/dm.ts
@@ -1,8 +1,33 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /**
- * dm 函数，调用主进程 dm 插件
+ * dm 对象
+ * 可以通过 use 函数调用主进程 dm 插件
+ * 也可以直接使用封装好的函数，以获得更好的提示
  */
 
-export default async function (method: string, args: any[]) {
-    return await window.ipcRenderer.invoke('dm', method, args)
+/**
+ * 通过 use 函数去调用主进程的大漠插件
+ * @param method 插件方法
+ * @param args 参数集合
+ * @returns
+ */
+async function use (method: string, args: any[]): Promise<any> {
+    const ret = await window.ipcRenderer.invoke('dm', method, args)
+    console.log(`-----${method}-----`, ret)
+    return ret
+}
+
+export default {
+    use,
+
+    // 系统 API
+    /**
+     * 运行指定的应用程序
+     * @param appPath 指定的可执行程序全路径，或程序名
+     * @param mode 0 : 普通模式 1 : 加强模式
+     * @returns 0 : 失败 1 : 成功
+     */
+    runApp: async (appPath: string, mode: 0 | 1): Promise<0 | 1> => {
+        return await use('RunApp', [appPath, mode])
+    },
 }
